837c0863f129665d89ee6c89e0404a4852436617
Glo Alba <glomontalba7@gmail.com>
2017-10-31 00:26:03 +0100

    Command_Line:parameters

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char **argv)
+{
+    
+   int arg1 = atoi(argv[1]);
+ 
+   
+   
+    
+    printf("The input number is: %d\n", arg1);
+    
+    
+	 return 0;
+}
+
