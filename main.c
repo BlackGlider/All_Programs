ee0fd50a9c4e3883ec6341a82f055f08eb2e2a5a
Glo Alba <glomontalba7@gmail.com>
2017-10-31 00:27:15 +0100

    Iterative_Function
    Factorial with Iterative Method

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
+#include<stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc,char **argv)
-{
+//ITERATIVE FUNCTION
+
+int main(int argc, char * argv[]) {
+   	int i;
+    long int fact=1;
     
-   int arg1 = atoi(argv[1]);
- 
    
-   
+    if(atoi(argv[1])==0||atoi(argv[1])==1)
+        fact=1;
+    else
+        for (i = 2; i <=atoi(argv[1]); i++)
+            fact=fact*i;
+    printf("Factorial of %s is %ld\n", argv[1],fact);
+    return 0;
     
-    printf("The input number is: %d\n", arg1);
-    
-    
-	 return 0;
 }
-
